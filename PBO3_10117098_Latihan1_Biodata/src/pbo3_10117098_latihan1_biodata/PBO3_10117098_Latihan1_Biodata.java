cafde39cfc1dcffbb74c7e2b08c4dd56dacccd30
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-09-22 23:58:54 +0700

    Add files via upload

---
+++ PBO3_10117098_Latihan1_Biodata/src/pbo3_10117098_latihan1_biodata/PBO3_10117098_Latihan1_Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan1_biodata;
+
+/**
+ *
+ * @author 
+ * NAMA  : Santi Susanti
+ * KELAS : PBO3
+ * NIM   : 10117098
+ */
+public class PBO3_10117098_Latihan1_Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("\t\t BIODATA" + "\n" + "-------------------------"
+                + "----------------");
+        System.out.println("NAMA\t\t: SANTI SUSANTI");
+        System.out.println("ALAMAT\t\t: Jalan Dipatiukur no 91A");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 13 April 1999");
+        System.out.println("-----------------------------------------");
+        System.out.println("HOBI\t\t: Belajar Bahasa Asing");
+        System.out.println("MAKES\t\t: Bakso");
+        System.out.println("MIKES\t\t: Milk Tea");
+        System.out.println("-----------------------------------------");
+    }
+    
+}
